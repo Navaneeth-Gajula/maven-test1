6d136db3f540e9169c419bf7ac9f44222eec4e40
Navaneeth-Gajula <navaneethgajula17@gmail.com>
2024-09-28 17:15:33 +0530

    NAVANEETHG0001 - Wrote the code for finding the area of the circle

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.java
@@ -0,0 +1,7 @@
+package main.java.com.example.javamavenjunithelloworld;
+
+public class Utility {
+    public static double findAreaOfCircle(double radius) {
+        return Math.PI * radius * radius;
+    }
+}
